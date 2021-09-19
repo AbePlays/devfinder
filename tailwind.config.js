d1e4f22ccd0847ed267cb9c5de1ad3ef753e6f9e
Abe <abhi.rawat456@gmail.com>
2021-09-19 17:42:28 +0530

    chore: added types

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /* eslint-disable */
 
 module.exports = {
-  mode: 'jit',
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
